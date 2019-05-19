def66726ebce5c946cd5204add74e351f3d1c494
Fayoke Adeyina <fayokeadeyina@Fayokes-MacBook-Pro.local>
2019-05-19 21:15:51 +0100

    -sort by id - sort by price - srot by size

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -15,50 +15,64 @@ import { Spinner } from '../commons/spinner';
  * @returns {ReactElement}
  */
 export class HomePage extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      faceProducts: {},
-      loading: this.props.loading
-    }
+  state = {
+    faceProducts: {},
+    productTag: ""
   }
 
   componentDidMount() {
-    const { getProducts } = this.props;
+    const { props } = this;
+    const { getProducts } = props;
     getProducts();
   }
 
   componentDidUpdate(prevProps, prevState) {
     const nextProps = this.props;
+    const nextState = this.state;
+
+    // update product state when product prop changes
     if (prevProps.products !== nextProps.products) {
       this.setProduct()
     }
-    if (prevProps.loading !== nextProps.loading) {
-      this.loadState();
+    // Calls getProduct when productTag state changes and filters by product tag
+    if (prevState.productTag !== nextState.productTag) {
+      const { getProducts } = this.props;
+      const {productTag} = this.state;
+      getProducts({productTag});
     }
   }
 
+  /**
+   * @method filterProduct
+   * @description filters product by size, price and id
+   */
+  filterProducts = (label) => {
+    this.setState({
+      productTag: label
+    })
+    return label;
+  }
+  /**
+   * @method setProduct
+   * @description sets the value of product from props to the state as faceProducts
+   */
   setProduct = () => {
     const { products } = this.props;
     this.setState({
       faceProducts: products
     })
   }
-  loadState = () => {
-    const { loading } = this.props;
-    this.setState({
-      loading
-    })
-  }
 
   render() {
-    const { faceProducts, loading } = this.state;
+    const { props, state, filterProducts } = this
+    const { faceProducts } = state;
+    const { loading } = props
     return (
       loading ?
         <Spinner />
         :
-        <Layout products={faceProducts} />
+        <Layout products={faceProducts} filterProducts={filterProducts} />
     )
   }
 }
@@ -69,7 +83,7 @@ export const mapStateToProps = state => ({
 });
 
 export const mapDispatchToProps = dispatch => ({
-  getProducts: () => dispatch(fetchProducts()),
+  getProducts: (value) => dispatch(fetchProducts(value)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
